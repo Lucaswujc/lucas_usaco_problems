335a820f15390a3f1f1c22410c037690b426cce8
Lucas_Wu <114350805+Username274@users.noreply.github.com>
2023-03-10 20:37:20 -0600

    lucas commit

--- debug.java
+++ debug.java
@@ -1,122 +1,130 @@
-/*
-ID: Lucas WU [lucaswu2]
-LANG: JAVA
-TASK: friday
-*/
 import java.io.*;
 import java.util.*;
+import java.lang.Math.*;
+import java.util.Arrays.*;
 
-public class debug {
-  public static void main (String [] args) throws IOException {
-    // Use BufferedReader rather than RandomAccessFile; it's much faster
-    BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
-    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("debug.out")));
-    //StringTokenizer st = new StringTokenizer(scan.readLine());
-    for(int i = 1; i <= 10000; i++){
-      System.out.print(i + 93324 + " ");
-    }
-    /*int t = Integer.parseInt(st.nextToken());
-    while(t>0){
-      st = new StringTokenizer(scan.readLine());
-      st = new StringTokenizer(scan.readLine());
-      int n = Integer.parseInt(st.nextToken());
-      int m = Integer.parseInt(st.nextToken());
-      Boolean ok =true;
-      String[][] cases = new String[m][2];
-      for(int i = 0; i < m; i++){
-        st = new StringTokenizer(scan.readLine());
-        cases[i][0] = st.nextToken();
-        cases[i][1] = st.nextToken();
-      }
-      for(int i =0; i <n; i++){
-        
-      }
-      if(ok){
-        System.out.println("OK");
-      }else{
-        System.out.println("LIE");
-      }
-      t--;
-    }
-    */
-  
-    
-    
-    
-    /* PROBLEM 2
-    int t = Integer.parseInt(st.nextToken());
-    while(t>0){
-      st = new StringTokenizer(scan.readLine());
-      int n = Integer.parseInt(st.nextToken());
-      int k = Integer.parseInt(st.nextToken());
-      st = new StringTokenizer(scan.readLine());
-      String s = st.nextToken();
-      char[] cows = new char[n];
-      char[] ans = new char[n];
-      for(int i = 0; i < n;i++){
-        cows[i] = s.charAt(i);
-        ans[i] = '.';
-      }
-      
-      int lastg = -k -1;
-      int lasth = -k-1;
-      int anspatches = 0;
-      for(int i = k; i < n; i++){
-        if(cows[i -k] == 'G'){
-          if((i - k) - lastg > k) {
-            anspatches++;
-            ans[i] = 'G';
-            lastg = i;
-            }
-        } 
-        else {
-          if ((i - k) - lasth > k) {
-              anspatches++;
-              ans[i] = 'H';
-              lasth = i;
-          }
-        }
-      }
-      Boolean extra = false;
-      for(int i = 0; i < n; i++){
-        if(cows[i] == 'G' && i -lastg > k){
-            extra = true;
-        }
-      }
-      if(extra){
-        for(int j = n-1; j >= 0; j--){
-          if(ans[j] == '.'){
-            anspatches++;
-            ans[j] = 'G';
-            break;
-          }
-        }
-      }
-      extra = false;
-      for(int i = 0; i <n; i++){
-        if(cows[i] == 'H' && i -lasth > k){
-            extra = true;
-        }
-      }
-      if(extra){
-      for(int j = n-1; j >= 0; j--){
-        if(ans[j] == '.'){
-            anspatches++;
-            ans[j] = 'H';
-            break;
-          }
-        }
-      }
-      System.out.println(anspatches);
-      for(int i =0; i < n; i++){
-        System.out.print(ans[i]);
-      }
-      System.out.println();
-      t--;
-    }   
-    */  
-    out.close();                         
+class Part {
+  int f;
+  int m;
+  int id;
+  public Part(int f, int m, int id) {
+    this.f = f;
+    this.m = m;
+    this.id = id;
   }
 }
 
-
+public class debug {
+  public static void main(String[] args) throws IOException {
+    // BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+    // StringTokenizer st = new StringTokenizer(scan.readLine());
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int[] x = new int[n];
+    int[] y = new int[n];
+    int[] sortedx = new int[n];
+    int[] sortedy = new int[n];
+    for(int i = 0; i <n ;i++){
+        x[i] = scan.nextInt();
+        y[i] = scan.nextInt();
+        sortedx[i] = x[i];
+        sortedy[i] = y[i];
+    }
+    Arrays.sort(sortedx);
+    Arrays.sort(sortedy);
+    int res = 2147483647;
+    for(int i =0; i <= Math.min(n-1,3); i++){
+        for(int j = Math.max(0, n-4); j >= n-1; j--){
+            for(int l = 0; l <= Math.min(n,3); l++){
+                for(int k = Math.max(0, n-4); k>=n-1; k--){
+                    int cnt = 0;
+                    for(int h =0; h <= n -1; h++){
+                        int x1 = x[h];
+                        int y1 = y[h];
+                        if ((x1 >= sortedx[i] && x1 <= sortedx[j]) && (y1 >= sortedy[l] && y1 <= sortedy[k])){
+                            cnt++;
+                        }
+                    }
+                    if(cnt>=n-3){
+                        res= Math.min(res, (sortedx[i] -sortedx[j])* (sortedy[l] - sortedy[k]));
+                    }
+                }
+            }
+        }
+    }
+    System.out.println(res);
+    /*
+     * PROBLEM 2
+     * int t = Integer.parseInt(st.nextToken());
+     * while(t>0){
+     * st = new StringTokenizer(scan.readLine());
+     * int n = Integer.parseInt(st.nextToken());
+     * int k = Integer.parseInt(st.nextToken());
+     * st = new StringTokenizer(scan.readLine());
+     * String s = st.nextToken();
+     * char[] cows = new char[n];
+     * char[] ans = new char[n];
+     * for(int i = 0; i < n;i++){
+     * cows[i] = s.charAt(i);
+     * ans[i] = '.';
+     * }
+     * 
+     * int lastg = -k -1;
+     * int lasth = -k-1;
+     * int anspatches = 0;
+     * for(int i = k; i < n; i++){
+     * if(cows[i -k] == 'G'){
+     * if((i - k) - lastg > k) {
+     * anspatches++;
+     * ans[i] = 'G';
+     * lastg = i;
+     * }
+     * }
+     * else {
+     * if ((i - k) - lasth > k) {
+     * anspatches++;
+     * ans[i] = 'H';
+     * lasth = i;
+     * }
+     * }
+     * }
+     * Boolean extra = false;
+     * for(int i = 0; i < n; i++){
+     * if(cows[i] == 'G' && i -lastg > k){
+     * extra = true;
+     * }
+     * }
+     * if(extra){
+     * for(int j = n-1; j >= 0; j--){
+     * if(ans[j] == '.'){
+     * anspatches++;
+     * ans[j] = 'G';
+     * break;
+     * }
+     * }
+     * }
+     * extra = false;
+     * for(int i = 0; i <n; i++){
+     * if(cows[i] == 'H' && i -lasth > k){
+     * extra = true;
+     * }
+     * }
+     * if(extra){
+     * for(int j = n-1; j >= 0; j--){
+     * if(ans[j] == '.'){
+     * anspatches++;
+     * ans[j] = 'H';
+     * break;
+     * }
+     * }
+     * }
+     * System.out.println(anspatches);
+     * for(int i =0; i < n; i++){
+     * System.out.print(ans[i]);
+     * }
+     * System.out.println();
+     * t--;
+     * }
+     */
+  }
+}
--- usaco3.java
+++ usaco3.java
@@ -1,69 +1,126 @@
 import java.io.*;
 import java.util.*;
+import java.lang.Math.*;
 
 public class usaco3 {
-  public static void main (String [] args) throws IOException {
-    BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
-    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("usaco3.out")));
-    StringTokenizer st = new StringTokenizer(scan.readLine());
-    int t = Integer.parseInt(st.nextToken());
-    while(t > 0){
+    /*
+     * Scan n & k
+     * Create array days of size n
+     * Scan n numbers
+     * Create Boolean Arr[n-1]
+     * loop from 0 to n-1
+     * if i is 0
+     * compare two days
+     * compare 2 + 2k & days[i+1]-days[i] + k
+     * if 2 + 2k is greater assign false to arr[i]
+     * assign true to arr[i] if there should be consecutive subscription
+     * if i != 0
+     * if(arr[i-1] is false)
+     * compare two days
+     * compare 2 + 2k & days[i+1]-days[i] + k
+     * if 2 + 2k is greater assign false to arr[i]
+     * assign true to arr[i] if there should be consecutive subscription
+     * 
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        // PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("usaco3.out")));
+        StringTokenizer st = new StringTokenizer(scan.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long k = Long.parseLong(st.nextToken());
+        long[] days = new long[(int) n];
         st = new StringTokenizer(scan.readLine());
-        String s = st.nextToken();
-        int olocate = -1;
-        if(s.length() <= 3){
-            olocate=1;
+        for (int i = 0; i < n; i++) {
+            days[i] = Long.parseLong(st.nextToken());
         }
-        else{
-            Boolean ok = false;
-            for(int i = 0; i < s.length()-3;i++){
-                if(((i < s.length()-3) && s.charAt(i+2) =='O') && (s.charAt(i) == 'M' && s.charAt(i+1) == 'O')){
-                    olocate = i + 1;
-                    ok = true;
-                    break;
-                }
-            }
-            if(!(ok)){
-                ok = false;
-                for(int i = 0; i < s.length()-2;i++){
-                    if(((i < s.length()-2 )) && (s.charAt(i) == 'M' && s.charAt(i+1) == 'O')){
-                        olocate = i + 1;
-                        ok =true;
-                        break;
-                    }
-                    if((i < s.length()-2 && i != 0) && (s.charAt(i) == 'O' && s.charAt(i+1) == 'O')){
-                        olocate = i;
-                        ok = true;
-                        break;
-                    }
-                }
-                if(!(ok)){
-                    for(int i = 0; i < s.length() -1;i++){
-                        if((s.charAt(i) == 'O' && i != 0)){
-                            olocate = i;
-                        }
-                    }                
-                }
-            }
-        }
-        if(olocate == -1 || s.length() <3 || s.charAt(olocate) == 'M'){
-            System.out.println(-1);
-        }
-        else{
-            int op = 0;
-            op = op + (olocate -1);
-            op = op + (s.length() - (olocate + 1)-1);
-            if(s.charAt(olocate -1) == 'O'){
-                op++;
-            }
-            if(s.charAt(olocate+1) == 'M'){
-                op++;
-            }
-            System.out.println(op);
-        }
-        t--;
+        daddySolution(days, n, k);
     }
-    out.close();                                  
-  }
-}
 
+    /**
+     * from days[] array, we will visit each and every element from index 0 to n-2
+     * at a location i, we will calculate days[i+1]-days[i], the difference of two days
+     * in the array representing the incremental cost if we want to include the days[i+1] in to 
+     * the current subscription. 
+     * if incrementalCost > k , then it is better of stop the current subscription and put the 
+     * days[i+1] into its own subscription . 
+     * We can use a variable to track the totalCost at each visit, the previous logic will change 
+     * the total cost depends on the decision. 
+     * Notice that when we visit the ith location in days[i], the days[i] is already considered in the subscription
+     * and we are deciding whether to put days[i+1] into the subsciption
+     *  
+     * @param days this array stores the day that Bessie want to watch movie
+     * @param n total number of days Bessie want to watch movies, n is the size of days array
+     * @param k fixed cost to start subsrciption 
+     */
+    public static void daddySolution(long[] days, long n, long k ){
+        long totalCost = k+1;//first element in the days array will always cost k+1 
+        for(int i = 0 ; i < n-1; i++){
+            long incrementalCost = days[i+1] - days[i];
+            if(incrementalCost > k){
+                //days[i+1] is in its own subscritption
+                totalCost = totalCost + k+1;
+            }else{
+                totalCost += incrementalCost;
+            }
+        }
+        System.out.println(totalCost);
+    }
+
+    /**
+     * use an array to remember consecutive subscription status as boolean
+     * iterate through the days array and fill the true/false into such array to 
+     * represent that at location i, true means days[i] should be in a consecutive subscription
+     * the size of this array is n-1 , and careful: we actually need to visit the nth elemnt 
+     * because the last element in days[n] can be a non-consecutive subscrition (very dangerous risky )
+     * @param n
+     * @param days
+     * @param k
+     */
+    public static void lucasSolution(int n, long[] days, long k) {
+        Boolean[] consec = new Boolean[(int) n - 1];
+        long money = 0;
+        for (int i = 0; i < n - 1; i++) {
+            // if i is 0
+            if (i == 0) {
+                if ((2 + 2 * k) < (days[i + 1] - days[i] + 1 + k)) {
+                    consec[i] = false;
+                } else {
+                    consec[i] = true;
+                }
+            } else { // if i is nto 0
+                if (consec[i - 1] == false) {
+                    if ((2 + 2 * k) < (days[i + 1] - days[i] + 1 + k)) {
+                        consec[i] = false;
+                    } else {
+                        consec[i] = true;
+                    }
+                } else {
+                    if ((1 + k) < (days[i + 1] - days[i])) {
+                        consec[i] = false;
+                    } else {
+                        consec[i] = true;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i != n - 1) {
+                if (consec[i] == false) {
+                    money += (1 + k);
+                } else {
+                    int temp = i;
+                    while (((i + 1) != n - 1) && consec[i + 1] == true) {
+                        i++;
+                    }
+                    i++;
+                    long add = (days[i] - days[temp] + 1 + k);
+                    money += add;
+                }
+            } else {
+                money += (1 + k);
+            }
+        }
+        System.out.println(money);
+        // out.close();
+    }
+}
