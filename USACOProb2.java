f35b9be140ecd73a519f95e6b05e1c4988970f24
Lucas_Wu <114350805+Username274@users.noreply.github.com>
2023-12-26 15:23:33 -0600

    safgjsak

---
+++ USACOProb2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.Math.*;
+import java.util.Arrays.*;
+import java.io.*;
+public class USACOProb2 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(scan.readLine());
+        int t = Integer.parseInt(st.nextToken());
+        while(t>0){
+            st = new StringTokenizer(scan.readLine());
+            int n = Integer.pariseInt(st.nextToken());
+            int c = Integer.pariseInt(st.nextToken());
+            int p = Integer.pariseInt(st.nextToken());
+            t--;
+        }
+    }
+}
