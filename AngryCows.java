99048aae76e8c196dae40ca531b512b88be8918e
Lucas_Wu <114350805+Username274@users.noreply.github.com>
2024-09-02 20:51:45 -0500

    secod commit for 20240902

---
+++ AngryCows.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class AngryCows {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("haybales.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("haybales.out")));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] haybales = new int[n];
+        for(int i = 0; i < n; i++ ){
+            haybales[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(haybales);
+        out.close();
+        br.close();
+    }
+}
